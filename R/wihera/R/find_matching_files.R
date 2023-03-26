cf4b0b1e2e4319afa1483750fc868c94f3412176
Ben Taft <ben.taft@career2cradle.org>
2023-03-26 13:36:56 -0500

    added file-finding utility

---
+++ R/wihera/R/find_matching_files.R
@@ -0,0 +1,24 @@
+#' Search a vector of file names for a pattern
+#'
+#' @param .pattern string - A regular expression to search for
+#' @param .files string - A vector of file names
+#'
+#' @return a single match, `NA`, or an error
+#' @export
+#'
+#' @examples
+#' find_matching_files("f", c("foo", "bar", "baz", "~$foo"))
+find_matching_files <- function(.pattern, .files){
+    detection <- stringr::str_subset(.files,
+                                     paste0("^(?!~).*",
+                                            .pattern))
+    if (length(detection) == 0) {
+        return(NA)
+    } else if (length(detection) > 1) {
+        cli::cli_abort(c(
+            "'{ .pattern }' matched multiple files:",
+            detection
+        ))
+    }
+    return(detection)
+}
